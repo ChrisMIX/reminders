7d137cb390e58a1af06e0ff3a109cba5cd4d4ffa
Chris Miklius <cmiklius@gmail.com>
2016-03-22 21:50:15 -0500

    edit environment file holdings

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -6,11 +6,10 @@ class RemindersController < ActionController::Base
     require 'twilio-ruby'
 
     # put your own credentials here
-    account_sid = 'AC99298024076004c694117fed9f33c42b'
-    auth_token = '8bbfc3cf65a00a3f00bfa55852324621'
+
 
     # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    @client = Twilio::REST::Client.new $account_sid, $auth_token
 
     @client.account.messages.create({
       :from => '+16305239409',
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+$account_sid = 'AC99298024076004c694117fed9f33c42b'
+$auth_token = '8bbfc3cf65a00a3f00bfa55852324621'
